e03f2478c3bb779b0e91b2e3d8ca1e14c81365c2
ajju06 <pewdikai.gamer@gmail.com>
2025-03-17 20:10:55 +0530

    app completed for deployment

---
+++ app/src/main/java/com/example/educationalbook/FY/ep_fy/ep_bca.java
@@ -0,0 +1,121 @@
+package com.example.educationalbook.FY.ep_fy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.educationalbook.FY.fy_main;
+import com.example.educationalbook.R;
+
+public class ep_bca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ImageView fyback;
+        ImageView sbcp;
+        ImageView sbfoc;
+        ImageView sbam;
+        ImageView sbbc;
+        ImageView sbco;
+        ImageView sbacp;
+        ImageView sbos;
+        ImageView sbdbms;
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ep_bca);
+        sbcp = findViewById(R.id.sb_cp);
+        sbfoc = findViewById(R.id.sb_foc);
+        sbam = findViewById(R.id.sb_am);
+        sbbc = findViewById(R.id.sb_bc);
+        sbco = findViewById(R.id.sb_co);
+        sbacp = findViewById(R.id.sb_acp);
+        sbos = findViewById(R.id.sb_os);
+        sbdbms = findViewById(R.id.sb_dbms);
+        fyback = findViewById(R.id.sb_back);
+
+        fyback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), fy_main.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbcp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_cp.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbfoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_foc.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_am.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbbc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_bc.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbacp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_acp.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_co.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_os_fy.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        sbdbms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ep_dbms.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/educationalbook/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.educationalbook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.educationalbook.FY.fy_main;
+import com.example.educationalbook.login_reg.login;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button btn;
+    TextView tview;
+    FirebaseUser user;
+    ImageView tenth_img;
+    ImageView twelfth_img;
+    ImageView fy_img;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        btn = findViewById(R.id.Logout);
+        user = auth.getCurrentUser();
+        tenth_img = findViewById(R.id.tenth);
+
+
+
+        tenth_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Tenth_Books.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        twelfth_img = findViewById(R.id.twelfth);
+        twelfth_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Twelfth_Books.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        fy_img = findViewById(R.id.FY);
+        fy_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), fy_main.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        if(user == null)
+        {
+            Intent i = new Intent(getApplicationContext(), login.class);
+            startActivity(i);
+            finish();
+        }
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(getApplicationContext(), login.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
